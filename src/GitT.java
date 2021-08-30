0ff3a38481b82dfb40dea8895ef0726eff9605ab
sksang <skoli35@gmail.com>
2021-08-30 12:48:29 +0530

    First commit

---
+++ src/GitT.java
@@ -0,0 +1,18 @@
+
+public class GitT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str1="Hello";
+		if(str1.equals("Hello"))
+		{
+			System.out.println("Equal");
+		}
+		else
+		{
+			System.out.println("Not Equal");
+		}
+	}
+
+}
